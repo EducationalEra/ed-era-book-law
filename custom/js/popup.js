8fc84193bb82b3301168e9745402491b50ae4d2f
igpersh <igpershukov@gmail.com>
2016-04-11 15:04:59 +0000

    Update custom/js/popup.js

--- custom/js/popup.js
+++ custom/js/popup.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
-   function init () {
-      $( "div.popup" ).replaceWith( "<div class='description'><div class='text'>Ще більше відеоматеріалів в <a class='knopka1' href='https://study.ed-era.com/courses/EdEra/m102/M102/about' target='_blank'>онлайн-курсі</a> від розробників цієї книги</div><div class='btn-knopka'><a class='knopka' href='https://study.ed-era.com/courses/EdEra/m102/M102/about'   target='_blank'>Перейти на курс</a></div>" );
-   }
+$( window ).load( function(){
+    function init () {
+        $( "div.popup" ).replaceWith( "<div class='description'><div class='text'>Ще більше відеоматеріалів в <a class='knopka1' href='https://study.ed-era.com/courses/EdEra/g102/G102/about' target='_blank'>онлайн-курсі</a> від розробників цієї книги</div><div class='btn-knopka'><a class='knopka' href='https://study.ed-era.com/courses/EdEra/g102/G102/about'   target='_blank'>Перейти на курс</a></div>" );
+        $("div.description").after( "<div class='space'></div>" );
+    }
 
-   init();
-   require(["gitbook"], function (gitbook) {
-      gitbook.events.bind("page.change", init);
-   });
+    init();
+    require(["gitbook"], function (gitbook) {
+        gitbook.events.bind("page.change", init);
+    });
 });
